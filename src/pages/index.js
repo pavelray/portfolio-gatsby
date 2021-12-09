bfd31f96878be262e1299e3b68a33a5ba4eb3658
Pavel Ray <paray@lowes.com>
2021-12-09 18:02:03 +0530

    Initial commit to my gatsby project

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,5 +1,20 @@
-import React from "react"
+import { Link } from "gatsby";
+import React from "react";
+import Layout from "../components/Layout";
+import {header , btn} from '../styles/home.module.css';
 
 export default function Home() {
-  return <div>Hello world!</div>
+  return (
+    <Layout>
+      <section className={header}>
+        <div>
+          <h2>Design</h2>
+          <h3>Develop & Deploy</h3>
+          <p>UX designer & web developer based in Manchester.</p>
+          <Link className={btn} to="/projects">My Portfolio Projects</Link>
+        </div>
+        <img src="./banner.png" alt="Banner Image" style={{maxWidth: '100%', maxHeight: '100%'}}/>
+      </section>
+    </Layout>
+  )
 }
