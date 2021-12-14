2e285afc38e377cca73f7752842870889d9bf2f5
Pavel Ray <paray@lowes.com>
2021-12-14 11:34:09 +0530

    added image plugin

--- src/pages/projects/index.jsx
+++ src/pages/projects/index.jsx
@@ -1,14 +1,42 @@
+import { graphql, Link } from "gatsby"
 import React from "react"
 import Layout from "../../components/Layout"
-import {portfolio} from '../../styles/projects.module.css'
+import { portfolio, projects } from "../../styles/projects.module.css"
 
-export default function Projects() {
+export default function Projects({ data }) {
+  const projectData = data.allMarkdownRemark.nodes
+
   return (
     <Layout>
       <div className={portfolio}>
         <h2>Portfolio</h2>
         <h3>Projects & Websites I've Created</h3>
+        <div className={projects}>
+          {projectData.map(project => (
+            <Link to={`/projects/${project.frontmatter.slug}`} id={project.id}>
+              <div>
+                <h3>{project.frontmatter.title}</h3>
+                <p>{project.frontmatter.stack}</p>
+              </div>
+            </Link>
+          ))}
+        </div>
       </div>
     </Layout>
   )
 }
+
+export const query = graphql`
+  query ProjectQuery {
+    allMarkdownRemark {
+      nodes {
+        frontmatter {
+          slug
+          stack
+          title
+        }
+        id
+      }
+    }
+  }
+`
